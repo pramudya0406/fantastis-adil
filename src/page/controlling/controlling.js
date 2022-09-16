0ef11ccbd937a30ccd880b57ecb2920381c86eb1
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-16 10:43:40 +0700

    tamplate folder

---
+++ src/page/controlling/controlling.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './controlling.css';
+const Controlling = () => {
+    return (
+        <>
+
+            <div>
+                Hallo
+            </div>
+
+        </>
+    )
+
+}
+export default Controlling
---
+++ src/page/dashboard/dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './dashboard.css'
+const Dashboard = () => {
+    return (
+        <>
+
+            <div>
+                Hallo
+            </div>
+
+        </>
+    )
+
+}
+export default Dashboard
