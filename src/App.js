14d2a4e2268513c30111c0a726d47ea907edfe6c
mayonice1424 <friyandrailham14@gmail.com>
2022-09-18 16:04:33 +0700

    Menambahkan halaman login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import Navigation from './component/navigation/navigation';
+import { Route } from 'react-router-dom';
+import Login from './page/login/login';
 
 function App() {
   return (
     <ChakraProvider>
-      <Navigation />
+      <Login />
     </ChakraProvider>
   );
 }
