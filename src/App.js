5aa03037cb7afc2861fb4e3a69ab88c8fbadf5d8
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-18 16:17:17 +0700

    boarding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
-import SideNav from './component/sidenav/sidenav';
 import Baord from './component/board/board';
 
 
--- src/page/dashboard/dashboard.js
+++ src/page/dashboard/dashboard.js
@@ -1,12 +1,36 @@
-import React from "react";
-import './dashboard.css'
+import React, { useState } from 'react'
+import {
+    Flex,
+    Image,
+    Box,
+    Center,
+    Text,
+    Icon,
+    calc,
+    Button,
+} from '@chakra-ui/react'
 const Dashboard = () => {
     return (
         <>
 
-            <div>
-                Hallo Dashboard
-            </div>
+            <Flex
+                w='100%'
+                flexDir={'column'}
+            >
+                <Flex
+                    w='100%'
+                    flexDir={'row'}
+                    justifyContent='space-between'
+                    alignItems={'center'}
+                    marginBottom='40px'
+                >
+                    <Text color={'black'}>
+                        List Greenhouse
+                    </Text>
+                    <Button bg='#14453E' size='sm' colorScheme={'teal'}>Tambah</Button>
+                </Flex>
+                <Text color={'black'}>hallo</Text>
+            </Flex>
 
         </>
     )
