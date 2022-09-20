a9d3bb58eb62032e14f59ceb6970ec68154416a9
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-20 13:34:32 +0700

    clear UI add in green house

--- src/component/card_greenhouse/card_green.js
+++ src/component/card_greenhouse/card_green.js
@@ -6,35 +6,79 @@ import {
     Text,
     Icon,
     WrapItem,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
 } from '@chakra-ui/react'
 import { RiDeleteBinFill, RiPencilFill, RiMapPinFill } from 'react-icons/ri';
 
 const CardGreenhouse = (props) => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     let data = props.data
     return (
-        <WrapItem>
-            <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                <Image src={data.image} h={'250px'} w={'350px'} />
+        <>
+            <WrapItem>
+                <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                    <Image src={data.image} h={'250px'} w={'350px'} />
 
-                <Flex p='2' justifyContent={'space-between'} flexDirection='row' w={'100%'} >
+                    <Flex p='2' justifyContent={'space-between'} flexDirection='row' w={'100%'} >
 
-                    <Text color={'black'}>
-                        {data.title}
-                    </Text>
-                    <Flex>
-                        <Icon as={RiDeleteBinFill} size={'24px'} color={'#B00020'} />
-                        <Icon as={RiPencilFill} size={'24px'} color={'#007BFF'} marginStart={'10px'} />
+                        <Text color={'black'}>
+                            {data.title}
+                        </Text>
+                        <Flex>
+                            <div onClick={() => {
+                                onOpen()
+                            }}>
+                                <Icon as={RiDeleteBinFill} size={'24px'} color={'#B00020'} />
+                            </div>
+                            <div>
+                                <Icon as={RiPencilFill} size={'24px'} color={'#007BFF'} marginStart={'10px'} />
+                            </div>
+                        </Flex>
+
                     </Flex>
+                    <Flex p='2' justifyItems={'center'}>
+                        <Icon as={RiMapPinFill} size={'30px'} color={'black'} />
+                        <Text color={'black'} flexDirection='row' w={'100%'} marginStart={'10px'}>
+                            {data.location}
+                        </Text>
+                    </Flex>
+                </Box>
+            </WrapItem>
 
-                </Flex>
-                <Flex p='2' justifyItems={'center'}>
-                    <Icon as={RiMapPinFill} size={'30px'} color={'black'} />
-                    <Text color={'black'} flexDirection='row' w={'100%'} marginStart={'10px'}>
-                        {data.location}
-                    </Text>
-                </Flex>
-            </Box>
-        </WrapItem>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        {'Hapus ' + data.title}
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text>
+                            {'Apakah kamu yakin menghapus ' + data.title}
+                        </Text>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button bg={'#E2E8F0'} mr={3} onClick={onClose}>
+                            Batal
+                        </Button>
+                        <Button variant='#09322D' bg={'#09322D'} color={'#ffff'} onClick={onClose}>
+                            Hapus
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
 export default CardGreenhouse
---
+++ src/page/greenhouse/greenhouse_add.js
@@ -0,0 +1,191 @@
+import React, { useState } from 'react'
+import {
+    Flex,
+    Text,
+    Button,
+    Wrap,
+    Input,
+    Image
+} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/form-control"
+import * as yup from "yup"
+import FilePicker from 'chakra-ui-file-picker'
+
+const GreenhouseAdd = () => {
+    let data = {
+        name: '',
+        location: '',
+    }
+
+    const [image, onChangeImage] = useState(null)
+
+
+    const schema = yup.object({
+        name: yup.string().required("data harus diisi"),
+        location: yup.string().required("data harus diisi"),
+        image: yup.object().required("data harus diisi"),
+    })
+
+    const submit = (name, location) => {
+        data.name = name
+        data.location = location
+
+        if (image == null || data.name == '' || data.location == '' || image == {} || image == '') {
+            return (alert('Masih ada yang belum di isi'))
+        }
+        else {
+            return (alert('mantap'))
+        }
+    }
+
+    return (
+        <>
+            <Flex
+                w='100%'
+                flexDir={'column'}
+            >
+                <Flex
+                    w='100%'
+                    flexDir={'row'}
+                    alignItems={'center'}
+                    marginBottom='70px'
+                >
+                    <Link to={'/unit/greenhouse'}>
+                        <Text color={'black'} marginRight={'20px'}>
+                            List Greenhouse
+                        </Text>
+                    </Link>
+                    <Text color={'black'} marginRight={'20px'}>{'>'}</Text>
+                    <Text color={'black'} >Tambah</Text>
+                </Flex>
+
+                <Flex
+                    w='100%'
+                    flexDir={'column'}
+                >
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            location: '',
+                            image: {}
+                        }}
+                        validationSchema={schema}
+                    >
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                        }) => (
+                            <Form>
+                                <FormControl marginTop={'20px'} isInvalid={errors.name && touched.name}>
+                                    <FormLabel htmlFor="name" color={'black'}>
+                                        Nama Greenhouse
+                                    </FormLabel>
+                                    <Input
+                                        width={'100%'}
+                                        h='60px'
+                                        maxWidth={'100%'}
+                                        marginTop={'0 auto'}
+                                        type="text"
+                                        name="name"
+                                        value={values.name}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        variant='outline'
+                                        placeholder="Masukkan email"
+                                        color={'black'}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.name}
+                                    </FormErrorMessage>
+                                </FormControl>
+
+                                <FormControl marginTop={'20px'} isInvalid={errors.location && touched.location}>
+                                    <FormLabel htmlFor="location" color={'black'}>
+                                        Nama Greenhouse
+                                    </FormLabel>
+                                    <Input
+                                        width={'100%'}
+                                        h='60px'
+                                        maxWidth={'100%'}
+                                        marginTop={'0 auto'}
+                                        type="text"
+                                        name="location"
+                                        value={values.location}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        variant='outline'
+                                        placeholder="Masukkan email"
+                                        color={'black'}
+                                    />
+                                    <FormErrorMessage>
+                                        {errors.location}
+                                    </FormErrorMessage>
+                                </FormControl>
+
+                                <FormControl marginTop={'20px'} isInvalid={errors.image && touched.image}>
+                                    <FormLabel htmlFor="image" color={'black'}>
+                                        Gambar Greenhouse
+                                    </FormLabel>
+                                    <Flex
+                                        width={'100%'}
+                                        h='100px'
+                                        borderRadius={'5px'}
+                                        maxWidth={'100%'}
+                                        marginTop={'0 auto'}
+                                        variant='outline'
+                                        placeholder="Masukkan email"
+                                        color={'black'}
+                                        alignItems='center'
+                                        borderWidth='1px'
+                                        borderColor={'#D9D9D9'}
+                                        padding={'20px'}
+                                    >
+                                        <FilePicker
+                                            onFileChange={(fileList) => onChangeImage(fileList)}
+                                            placeholder="Pilih Gambar"
+                                            clearButtonLabel="Hapus"
+                                            multipleFiles={true}
+                                            accept="image/*"
+                                            hideClearButton={false}
+                                        />
+                                    </Flex>
+                                    <FormErrorMessage>
+                                        {errors.image}
+                                    </FormErrorMessage>
+                                </FormControl>
+                                <Button
+                                    marginTop={'44px'}
+                                    width="100%"
+                                    height="50px"
+                                    borderRadius="10px"
+                                    backgroundColor="var(--color-primer)"
+                                    type="submit"
+                                    // className="btn-login"
+                                    onClick={() => submit(values.image, values.location)}
+                                >
+                                    <Text
+                                        fontWeight='bold'
+                                        fontFamily='var(--font-family-secondary)'
+                                        fontSize='var(--header-3)'
+                                        color='var(--color-on-primary)'
+                                        colorScheme={'var(--color-on-primary)'}
+                                    >
+                                        Tambah
+                                    </Text>
+                                </Button>
+                            </Form>
+                        )}
+                    </Formik>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
+
+export default GreenhouseAdd
