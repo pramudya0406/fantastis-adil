ced130f37558776d2cbe1745ada5c6add8d34582
dhifafaz <dhifaf.az@gmail.com>
2022-09-23 08:11:51 +0700

    add new style for nav item

--- src/component/navitem/navitem.js
+++ src/component/navitem/navitem.js
@@ -1,52 +1,56 @@
-import React from 'react'
+import React from "react";
 import {
-    Flex,
-    Text,
-    Icon,
-    Link,
-    Menu,
-    MenuButton,
-    MenuList,
-    Center
-} from '@chakra-ui/react'
+	Flex,
+	Text,
+	Icon,
+	Link,
+	Menu,
+	MenuButton,
+	MenuList,
+	Center,
+} from "@chakra-ui/react";
+import "../sidenav/navigation.css";
 
 export default function NavItem({ icon, title, description, active, navSize }) {
-
-    return (
-        <Flex
-            mt={30}
-            flexDir="column"
-            w="100%"
-            alignContent={'center'}
-            justifyContent={'center'}
-            alignItems={navSize == "small" ? "center" : "flex-start"}
-        >
-            <Menu placement="right">
-                <Flex
-                    backgroundColor={active && "#FFFF"}
-                    p={3}
-                    borderRadius={8}
-                    _hover={{ textDecor: '#FFFF', backgroundColor: "#FBFBFB" }}
-                    w={navSize == "large" && "100%"}
-                    alignContent={'center'}
-                    alignItems={'center'}
-                >
-                    <MenuButton w="100%" >
-                        <Flex alignItems={'center'}>
-                            <Icon fontWeight={'bold'} fontSize={'var(--header-3)'} as={icon} color={active ? 'var(--color-primer)' : 'var(--color-grey)'} />
-                            <Text
-                                ml={5}
-                                display={navSize == "small" ? "none" : "flex"}
-                                fontWeight={'semibold'}
-                                fontSize={'var(--header-3)'}
-                                color={active ? 'var(--color-primer)' :  'var(--color-grey)'}>
-                                {title}
-                            </Text>
-                        </Flex>
-                    </MenuButton>
-                </Flex>
-
-            </Menu>
-        </Flex>
-    )
+	return (
+		<Flex
+			mt={30}
+			flexDir="column"
+			w="100%"
+			alignContent={"center"}
+			justifyContent={"center"}
+			alignItems={navSize == "small" ? "center" : "flex-start"}>
+			<Menu placement="right">
+				<Flex
+					backgroundColor={active && "#FFFF"}
+					p={3}
+					borderLeftRadius={25}
+                    // borderRightRadius={-25}
+					_hover={{ textDecor: "#FFFF", backgroundColor: "#FBFBFB" }}
+					w={navSize == "large" && "100%"}
+					alignContent={"center"}
+					alignItems={"center"}
+					className="nav-item">
+					<MenuButton w="100%">
+						<Flex alignItems={"center"}>
+							<Icon
+								fontWeight={"bold"}
+								fontSize={"var(--header-3)"}
+								as={icon}
+								color={active ? "var(--color-primer)" : "var(--color-grey)"}
+							/>
+							<Text
+								ml={5}
+								display={navSize == "small" ? "none" : "flex"}
+								fontWeight={"semibold"}
+								fontSize={"var(--header-3)"}
+								color={active ? "var(--color-primer)" : "var(--color-grey)"}>
+								{title}
+							</Text>
+						</Flex>
+					</MenuButton>
+				</Flex>
+			</Menu>
+		</Flex>
+	);
 }
--- src/component/sidenav/sidenav.js
+++ src/component/sidenav/sidenav.js
@@ -1,100 +1,113 @@
-import React, { useState } from 'react'
-import './navigation.css';
-import {
-    Flex,
-    Image,
-    Box,
-    Center,
-} from '@chakra-ui/react'
-import {
-    FiHome,
-    FiMonitor,
-} from 'react-icons/fi'
-import {
-    GiGreenhouse
-} from 'react-icons/gi'
-import {
-    AiOutlineControl,
-    AiOutlineHistory
-} from 'react-icons/ai'
-import NavItem from '../navitem/navitem'
-import { useSelector, useDispatch } from 'react-redux';
-import { routePageName } from '../../redux/action';
+import React, { useState } from "react";
+import "./navigation.css";
+import { Flex, Image, Box, Center } from "@chakra-ui/react";
+import { FiHome, FiMonitor } from "react-icons/fi";
+import { GiGreenhouse } from "react-icons/gi";
+import { AiOutlineControl, AiOutlineHistory } from "react-icons/ai";
+import NavItem from "../navitem/navitem";
+import { useSelector, useDispatch } from "react-redux";
+import { routePageName } from "../../redux/action";
 import { Link } from "react-router-dom";
 
 const SideNav = () => {
-    const navSize = "large"
+	const navSize = "large";
 
-    const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-    const patchRoute = (data) => {
-        dispatch(routePageName(data))
-    }
-    const { routeName } = useSelector(
-        state => state.userReducer,
-    );
+	const patchRoute = (data) => {
+		dispatch(routePageName(data));
+	};
+	const { routeName } = useSelector((state) => state.userReducer);
 
-    return (
-        <>
-            <Flex
-                backgroundColor={'#09322D'}
-                display={{
-                    base: 'none',
-                    lg: 'flex',
+	return (
+		<>
+			<Flex
+				backgroundColor={"#09322D"}
+				display={{
+					base: "none",
+					lg: "flex",
+				}}
+				pos="sticky"
+				h="100vh"
+				boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+				w={"370px"}
+				flexDir="column"
+				justifyContent="space-between">
+				<Flex
+					// p="5%"
+					flexDir="column"
+					w="100%"
+					as="nav"
+					className="navbar">
+					<Box paddingRight="5%">
+						<Center>
+							<Image src="https://res.cloudinary.com/diyu8lkwy/image/upload/v1663396824/itera%20herro%20icon/Frame_9_1_sznmbk.png" />
+						</Center>
+					</Box>
 
-                }}
-
-                pos="sticky"
-                h="100vh"
-                boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-                w={'370px'}
-                flexDir="column"
-                justifyContent="space-between"
-            >
-                <Flex
-                    p="5%"
-                    flexDir="column"
-                    w="100%"
-                    as="nav"
-                >
-                    <Box >
-                        <Center>
-                            <Image src='https://res.cloudinary.com/diyu8lkwy/image/upload/v1663396824/itera%20herro%20icon/Frame_9_1_sznmbk.png' />
-                        </Center>
-                    </Box>
-
-                    <Link to={'/unit/dashboard'} onClick={() => {
-                        patchRoute('Dashboard')
-                    }}>
-                        <NavItem navSize={navSize} icon={FiHome} title="Dashboard" active={routeName === 'Dashboard'} />
-                    </Link>
-                    <Link to={'/unit/greenhouse'} onClick={() => {
-                        patchRoute('Greenhouse')
-                    }}>
-                        <NavItem navSize={navSize} icon={GiGreenhouse} title="Greenhouse" active={routeName === 'Greenhouse'} />
-                    </Link>
-                    <Link to={'/unit/monitoring'} onClick={() => {
-                        patchRoute('Monitoring')
-                    }}>
-                        <NavItem navSize={navSize} icon={FiMonitor} title="Monitoring" active={routeName === 'Monitoring'} />
-                    </Link>
-                    <Link to={'/unit/controlling'} onClick={() => {
-                        patchRoute('Controlling')
-                    }}>
-                        <NavItem navSize={navSize} icon={AiOutlineControl} title="Controlling" active={routeName === 'Controlling'} />
-                    </Link>
-                    <Link to={'/unit/historynotifikasi'} onClick={() => {
-                        patchRoute('History Notification')
-                    }}>
-                        <NavItem navSize={navSize} icon={AiOutlineHistory} title="History Notification" active={routeName === 'History Notification'} />
-                    </Link>
-
-                </Flex>
-
-            </Flex>
-
-        </>
-    )
-
-}
-export default SideNav
+					<Link
+						to={"/unit/dashboard"}
+						onClick={() => {
+							patchRoute("Dashboard");
+						}}>
+						<NavItem
+							navSize={navSize}
+							icon={FiHome}
+							title="Dashboard"
+							active={routeName === "Dashboard"}
+						/>
+					</Link>
+					<Link
+						to={"/unit/greenhouse"}
+						onClick={() => {
+							patchRoute("Greenhouse");
+						}}>
+						<NavItem
+							navSize={navSize}
+							icon={GiGreenhouse}
+							title="Greenhouse"
+							active={routeName === "Greenhouse"}
+						/>
+					</Link>
+					<Link
+						to={"/unit/monitoring"}
+						onClick={() => {
+							patchRoute("Monitoring");
+						}}>
+						<NavItem
+							navSize={navSize}
+							icon={FiMonitor}
+							title="Monitoring"
+							active={routeName === "Monitoring"}
+						/>
+					</Link>
+					<Link
+						to={"/unit/controlling"}
+						onClick={() => {
+							patchRoute("Controlling");
+						}}>
+						<NavItem
+							navSize={navSize}
+							icon={AiOutlineControl}
+							title="Controlling"
+							active={routeName === "Controlling"}
+						/>
+					</Link>
+					<Link
+						to={"/unit/historynotifikasi"}
+						onClick={() => {
+							patchRoute("History Notification");
+						}}>
+						<NavItem
+							navSize={navSize}
+							icon={AiOutlineHistory}
+							title="History Notification"
+							active={routeName === "History Notification"}
+						/>
+					</Link>
+				</Flex>
+			</Flex>
+		</>
+	);
+};
+export default SideNav;
