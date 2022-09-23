7c5a58fe14b24924f63058453fc91e98071a316d
dhifafaz <dhifaf.az@gmail.com>
2022-09-23 20:01:55 +0700

    fixed conflict

--- src/component/card_greenhouse/card_green.js
+++ src/component/card_greenhouse/card_green.js
@@ -34,7 +34,12 @@ const CardGreenhouse = (props) => {
 						justifyContent={"space-between"}
 						flexDirection="row"
 						w={"100%"}>
-						<Text color={"black"}>{data.title}</Text>
+						<Text
+							fontWeight={"semibold"}
+							fontSize={"var(--header-3)"}
+							color={"black"}>
+							{data.title}
+						</Text>
 						<Flex>
 							<div
 								onClick={() => {
@@ -56,11 +61,12 @@ const CardGreenhouse = (props) => {
 						</Flex>
 					</Flex>
 					<Flex p="2" justifyItems={"center"}>
-						<Icon as={RiMapPinFill} w={18} h={18} color={"black"} />
+						<Icon as={RiMapPinFill} size={"30px"} color={"black"} />
 						<Text
 							color={"black"}
 							flexDirection="row"
 							w={"100%"}
+							fontSize={"var(--header-5)"}
 							marginStart={"10px"}>
 							{data.location}
 						</Text>
@@ -68,59 +74,30 @@ const CardGreenhouse = (props) => {
 				</Box>
 			</WrapItem>
 
+			<Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>{"Hapus " + data.title}</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<Text>{"Apakah kamu yakin menghapus " + data.title}</Text>
+					</ModalBody>
 
-                    <Flex p='2' justifyContent={'space-between'} flexDirection='row' w={'100%'} >
-                        <Text fontWeight={'semibold'} fontSize={'var(--header-3)'} color={'black'}>
-                            {data.title}
-                        </Text>
-                        <Flex>
-                            <div onClick={() => {
-                                onOpen()
-                            }}>
-                                <Icon as={RiDeleteBinFill} size={'24px'} color={'#B00020'} />
-                            </div>
-                            <Link to={{
-                                pathname: '/unit/greenhouse/' + data.title
-                            }}>
-                                <Icon as={RiPencilFill} size={'24px'} color={'#007BFF'} marginStart={'10px'} />
-                            </Link >
-                        </Flex>
-
-                    </Flex>
-                    <Flex p='2' justifyItems={'center'}>
-                        <Icon as={RiMapPinFill} size={'30px'} color={'black'} />
-                        <Text color={'black'} flexDirection='row' w={'100%'} fontSize={'var(--header-5)'} marginStart={'10px'}>
-                            {data.location}
-                        </Text>
-                    </Flex>
-                </Box>
-            </WrapItem>
-
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>
-                        {'Hapus ' + data.title}
-                    </ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Text>
-                            {'Apakah kamu yakin menghapus ' + data.title}
-                        </Text>
-                    </ModalBody>
-
-                    <ModalFooter>
-                        <Button bg={'#E2E8F0'} mr={3} onClick={onClose}>
-                            Batal
-                        </Button>
-                        <Button variant='#09322D' bg={'#09322D'} color={'#ffff'} onClick={onClose}>
-                            Hapus
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </>
-    )
-}
-export default CardGreenhouse
-
+					<ModalFooter>
+						<Button bg={"#E2E8F0"} mr={3} onClick={onClose}>
+							Batal
+						</Button>
+						<Button
+							variant="#09322D"
+							bg={"#09322D"}
+							color={"#ffff"}
+							onClick={onClose}>
+							Hapus
+						</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+export default CardGreenhouse;
