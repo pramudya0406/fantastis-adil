f43ca0ea7b72935a5b0c3754ddf80e3e237436fd
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-27 05:24:56 +0700

    login clear

--- src/page/login/login.js
+++ src/page/login/login.js
@@ -1,9 +1,11 @@
+import React, { useEffect } from "react"
 import { Box, Flex, Button, Image, Text, Input } from "@chakra-ui/react"
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/form-control"
 import * as yup from "yup"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { TabTitle } from "../../Utility/utility"
+import axios from "axios"
 const schema = yup.object({
 
   email: yup
@@ -16,6 +18,36 @@ const schema = yup.object({
 })
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmitComplate = (emailValue, passwordValue) => {
+    axios.post('https://iterahero.herokuapp.com/api/v1/login', {
+      email: emailValue,
+      password: passwordValue,
+    })
+      .then((response) => {
+        if (response.data == '' || response.data == ' ') {
+          alert('Login gagal')
+        }
+        else {
+          localStorage.setItem('token', response.data.data.accessToken)
+        }
+      })
+      .catch((error) => console.log(error))
+  }
+
+  const checkToken = () => {
+    if (localStorage.getItem('token') != null || localStorage.getItem('token') != '') {
+      navigate('/unit/dashboard')
+    }
+  }
+
+  useEffect(() => {
+    checkToken()
+
+  }, [])
+
+
   TabTitle("Login - ITERA Hero")
   return (
 
@@ -87,24 +119,24 @@ const Login = () => {
                     {errors.password}
                   </FormErrorMessage>
                 </FormControl>
-                <Link to={"/unit/dashboard"}>
-                  <Button
-                    marginTop={'44px'}
-                    width="100%"
-                    height="50px"
-                    borderRadius="10px"
-                    backgroundColor="var(--color-primer)"
+                {/* <Link to={"/unit/dashboard"}> */}
+                <Button
+                  marginTop={'44px'}
+                  width="100%"
+                  height="50px"
+                  borderRadius="10px"
+                  backgroundColor="var(--color-primer)"
 
-                    type="submit"
-                    className="btn-login"
-                    onClick={() => {
-                      handleSubmit();
-                    }}>
-                    <Text fontWeight='bold' fontFamily='var(--font-family-secondary)' fontSize='var(--header-3)' color='var(--color-on-primary)' >
-                      Masuk
-                    </Text>
-                  </Button>
-                </Link>
+                  type="submit"
+                  className="btn-login"
+                  onClick={() => {
+                    handleSubmitComplate(values.email, values.password)
+                  }}>
+                  <Text fontWeight='bold' fontFamily='var(--font-family-secondary)' fontSize='var(--header-3)' color='var(--color-on-primary)' >
+                    Masuk
+                  </Text>
+                </Button>
+                {/* </Link> */}
               </Form>
             )}
           </Formik>
