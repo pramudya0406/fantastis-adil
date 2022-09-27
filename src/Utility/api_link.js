ccca7bd03c5df18dda2872cc75000e8c902f9e4a
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-27 09:00:00 +0700

    consume greenhouse list

--- src/Utility/api_link.js
+++ src/Utility/api_link.js
@@ -1,2 +1,3 @@
 export const loginApi = 'https://iterahero.herokuapp.com/api/v1/login'
 export const dashboardApi = 'https://iterahero.herokuapp.com/api/v1/dashboard'
+export const listGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse'
