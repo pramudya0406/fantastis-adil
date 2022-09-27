5e075319bf4e5cd7ea77118f91b053168ef32c4b
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-27 14:11:08 +0700

    consume api add greenhouse

--- src/Utility/api_link.js
+++ src/Utility/api_link.js
@@ -1,3 +1,4 @@
 export const loginApi = 'https://iterahero.herokuapp.com/api/v1/login'
 export const dashboardApi = 'https://iterahero.herokuapp.com/api/v1/dashboard'
-export const listGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse'
+export const listGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse?size=100'
+export const addGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse'
