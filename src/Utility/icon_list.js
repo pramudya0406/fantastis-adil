52d37cdc589b1846978fd065594b911e5d80bac1
FARRAS-DARKUNO <abdurrachman.farras2@gmail.com>
2022-09-28 11:56:05 +0700

    icon and color list

---
+++ src/Utility/icon_list.js
@@ -0,0 +1,103 @@
+let iconsList = [
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663905296/itera%20herro%20icon/icon-notif_owss6p.png',
+        color: '#09322D'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/Lovepik_com-401124754-water-droplets_1_wwcjby.png',
+        color: '#73D1FA'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/Lovepik_com-401703801-stone_1_n8qix0.png',
+        color: '#A54D26'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/Lovepik_com-400222655-test-tube_1_jhq5uo.png',
+        color: '#EE7C8B'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/Lovepik_com-832199744-25D_colorful_pump_can_be_used_as_a_commercial_element_1_upljlx.png',
+        color: '#73D1FA'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/Lovepik_com-832199744-25D_colorful_pump_can_be_used_as_a_commercial_element_1_upljlx.png',
+        color: '#FFEF00'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/pngegg_1_aeolob.png',
+        color: '#1775BB'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/pngkey_1_gegauo.png',
+        color: '#349F4B'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/pngkey_2_hitbag.png',
+        color: '#FF0000'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/pngkey_3_zob6my.png',
+        color: '#000000'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229870/itera%20herro%20icon/pngkey_4_bgyegr.png',
+        color: '#000000'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229869/itera%20herro%20icon/pngkey_5_v6jzws.png',
+        color: '#A6CE39'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229869/itera%20herro%20icon/lucide_pipette_diqu1b.png',
+        color: '#007BFF'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229869/itera%20herro%20icon/iwwa_thermometer_britm9.png',
+        color: '#319795'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1663229869/itera%20herro%20icon/healthicons_thermometer_censrx.png',
+        color: '#F24E1E'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664329448/itera%20herro%20icon/akar-icons_water_o8epga.png',
+        color: '#007BFF'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664327875/itera%20herro%20icon/material-symbols_mode-fan_vb9uv6.png',
+        color: '#57A519'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664327875/itera%20herro%20icon/ic_baseline-water_pb5en2.png',
+        color: '#319795'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664327874/itera%20herro%20icon/ic_baseline-water_1_r0pr48.png',
+        color: '#FFB801'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664327874/itera%20herro%20icon/color-picker_ivzmog.png',
+        color: '#AA56FF'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664329491/itera%20herro%20icon/mdi_water-pump_ddpvel.png',
+        color: '#007BFF'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664329514/itera%20herro%20icon/teenyicons_plant-solid_xbrcle.png',
+        color: '#349F4B'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664329522/itera%20herro%20icon/sun_slayag.png',
+        color: '#B7A925'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664329528/itera%20herro%20icon/thermometer_llxtab.png',
+        color: '#0086D1'
+    },
+    {
+        icon: 'https://res.cloudinary.com/diyu8lkwy/image/upload/v1664329540/itera%20herro%20icon/material-symbols_water-damage-rounded_twyv5g.png',
+        color: '#007BFF'
+    },
+
+]
