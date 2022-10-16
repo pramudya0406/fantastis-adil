ba7c186b726b16e0c48719cf6423311c203e5806
dhifafaz <dhifaf.az@gmail.com>
2022-10-16 20:32:26 +0700

    delete notification done

--- src/Utility/api_link.js
+++ src/Utility/api_link.js
@@ -1,20 +1,34 @@
-
-export const loginApi = 'https://iterahero.herokuapp.com/api/v1/login'
-export const dashboardApi = 'https://iterahero.herokuapp.com/api/v1/dashboard'
-export const greenhouseByUserId = 'https://iterahero.herokuapp.com/api/v1/greenhouse'
-export const controllingApi = 'https://iterahero.herokuapp.com/api/v1/actuator?by_greenhouse_id='
-export const monitoringApi = 'https://iterahero.herokuapp.com/api/v1/sensor?by_id_greenhouse='
-export const listGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse?size=100'
-export const addGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse'
-export const deleteGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse/'
-export const updateGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse/'
-export const getApiGreenhouse = 'https://iterahero.herokuapp.com/api/v1/greenhouse/'
-export const categoryApi = 'https://iterahero.herokuapp.com/api/v1/category/sensor'
-export const addSensorApi = 'https://iterahero.herokuapp.com/api/v1/sensor'
-export const addActuatorApi = 'https://iterahero.herokuapp.com/api/v1/actuator'
-export const deleteAktuatorApi = 'https://iterahero.herokuapp.com/api/v1/actuator/'
-export const deleteSensorApi = 'https://iterahero.herokuapp.com/api/v1/sensor/'
-export const paginationMonitoring ='https://iterahero.herokuapp.com/api/v1/sensor?by_id_greenhouse='
-export const paginationAktuator ='https://iterahero.herokuapp.com/api/v1/actuator?by_greenhouse_id='
+export const loginApi = "https://iterahero.herokuapp.com/api/v1/login";
+export const dashboardApi = "https://iterahero.herokuapp.com/api/v1/dashboard";
+export const greenhouseByUserId =
+	"https://iterahero.herokuapp.com/api/v1/greenhouse";
+export const controllingApi =
+	"https://iterahero.herokuapp.com/api/v1/actuator?by_greenhouse_id=";
+export const monitoringApi =
+	"https://iterahero.herokuapp.com/api/v1/sensor?by_id_greenhouse=";
+export const listGreenhouse =
+	"https://iterahero.herokuapp.com/api/v1/greenhouse?size=100";
+export const addGreenhouse =
+	"https://iterahero.herokuapp.com/api/v1/greenhouse";
+export const deleteGreenhouse =
+	"https://iterahero.herokuapp.com/api/v1/greenhouse/";
+export const updateGreenhouse =
+	"https://iterahero.herokuapp.com/api/v1/greenhouse/";
+export const getApiGreenhouse =
+	"https://iterahero.herokuapp.com/api/v1/greenhouse/";
+export const categoryApi =
+	"https://iterahero.herokuapp.com/api/v1/category/sensor";
+export const addSensorApi = "https://iterahero.herokuapp.com/api/v1/sensor";
+export const addActuatorApi = "https://iterahero.herokuapp.com/api/v1/actuator";
+export const deleteAktuatorApi =
+	"https://iterahero.herokuapp.com/api/v1/actuator/";
+export const deleteSensorApi = "https://iterahero.herokuapp.com/api/v1/sensor/";
+export const paginationMonitoring =
+	"https://iterahero.herokuapp.com/api/v1/sensor?by_id_greenhouse=";
+export const paginationAktuator =
+	"https://iterahero.herokuapp.com/api/v1/actuator?by_greenhouse_id=";
 export const getNotificationByUserId =
 	"https://iterahero.herokuapp.com/api/v1/notification?by_user_id=1";
+
+export const deleteNotification =
+	"https://iterahero.herokuapp.com/api/v1/notification/";
--- src/page/controlling/controlling.js
+++ src/page/controlling/controlling.js
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./controlling.css";
-import {
-	Text,
-	Button,
-	Select,
-	Flex,
-	Form,
-} from "@chakra-ui/react";
+import { Text, Button, Select, Flex, Form } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { TabTitle } from '../../Utility/utility';
+import { TabTitle } from "../../Utility/utility";
 import { useDispatch } from "react-redux";
 import { routePageName } from "../../redux/action";
 import axios from "axios";
@@ -19,125 +13,134 @@ import TableControlling from "../../component/table/controlling_table";
 import { Formik } from "formik";
 
 const Controlling = () => {
-TabTitle("Controlling - ITERA Hero");
-const navigate = useNavigate();
-const [dataApi, setDataApi] = useState(null)
-const [data, setData] = useState('')
-const header = localStorage.getItem('token')
+	TabTitle("Controlling - ITERA Hero");
+	const navigate = useNavigate();
+	const [dataApi, setDataApi] = useState(null);
+	const [data, setData] = useState("");
+	const header = localStorage.getItem("token");
 
-const getApiGreenhouse = async () => {
-    await axios.get(greenhouseByUserId, {
-        headers: {
-            'Authorization': 'Bearer ' + header
-        }
-    })
-        .then(response => setDataApi(response.data.data))
-        .catch((error) => {
-            localStorage.clear()
-            navigate('/login')
-        })
-}
+	const getApiGreenhouse = async () => {
+		await axios
+			.get(greenhouseByUserId, {
+				headers: {
+					Authorization: "Bearer " + header,
+				},
+			})
+			.then((response) => setDataApi(response.data.data))
+			.catch((error) => {
+				localStorage.clear();
+				navigate("/login");
+			});
+	};
 	const dispatch = useDispatch();
 	useEffect(() => {
-        getApiGreenhouse()
+		getApiGreenhouse();
 		return () => {
 			dispatch(routePageName("Controlling"));
 		};
 	}, []);
 	return (
-        <>
-        {dataApi == null ? <Loading/>
-		:<Flex gap={"30px"} width={"100%"} flexDir={"column"}>
-			<Flex justifyContent={"space-between"} width="100%">
-				<Link>
-					<Text
-						fontWeight={"semibold"}
-						fontSize={"var(--header-3)"}
-						color={"var(--color-primer)"}>
-						List Controlling pada Greenhouse
-					</Text>
-				</Link>
-			</Flex>
-			<Flex
-				alignContent={"center"}
-				alignItems={"center"}
-				justifyContent={"space-between"}>
-				<Flex width={"30%"}>
-					<Formik
-						initialValues={{
-							greenhouse: "",
-						}}
-						onSubmit={(values) => {
-							setData(values.greenhouse)
-						}}>
-						{({
-							values,
-							handleChange,
-							handleSubmit,
-							isSubmitting,
-							setFieldValue,
-						}) => (
-							<form onSubmit={handleSubmit}>
-								<Flex alignContent={"center"}
-											alignItems={"center"}
-											justify={"space-between"}> 
-								<Flex width={"100%"}>
-									<Select
-										onChange={(e) => {
-											setFieldValue('id', e.target.value);
-											setData(e.target.value)
-										}}
-										size="xs"
-										borderRadius={"10"}
-										name="greenhouse"
-										value={values.id}
-										placeholder="Pilih Greenhouse"
-										width={"100%"}
-										bg={"white"}
-										_active={{ bg: "white" }}
-										borderColor={"var(--color-border)"}
-										fontSize={"var(--header-5)"}
-										fontWeight={"normal"}
-										color={"var(--color-primer)"}
-										_hover={{ borderColor: "var(--color-border)" }}
-										_focusWithin={{ borderColor: "var(--color-border)" }}>
-										{dataApi.map((item, index) => {
-											return (
-												<option
-													color={"var(--color-border)"}
-													key={index}
-													value={item.id}>
-													{item.name}
-												</option>
-											);
-										})}
-									</Select>
-								</Flex>
-								</Flex>
-							</form>
-						)}
-					</Formik>
-				</Flex>
-				{
-							data === '' ? <></> : 										
-						<Link to={"/unit/controlling/add/" + data}>
-							<Button
-							  data = {{name:data}}
-								type="submit"
-								bg={"var(--color-primer)"}
-								>
-								Tambah
-							</Button>
+		<>
+			{dataApi == null ? (
+				<Loading />
+			) : (
+				<Flex gap={"30px"} width={"100%"} flexDir={"column"}>
+					<Flex justifyContent={"space-between"} width="100%">
+						<Link>
+							<Text
+								fontWeight={"semibold"}
+								fontSize={"var(--header-3)"}
+								color={"var(--color-primer)"}>
+								List Controlling pada Greenhouse
+							</Text>
 						</Link>
-						}
-			</Flex>
-			{data === '' ? <></> : <TableControlling data={{
-				id : data
-			}} />
-		}
-		</Flex>
-				}
-				</>
+					</Flex>
+					<Flex
+						alignContent={"center"}
+						alignItems={"center"}
+						justifyContent={"space-between"}>
+						<Flex width={"30%"}>
+							<Formik
+								initialValues={{
+									greenhouse: "",
+								}}
+								onSubmit={(values) => {
+									setData(values.greenhouse);
+								}}>
+								{({
+									values,
+									handleChange,
+									handleSubmit,
+									isSubmitting,
+									setFieldValue,
+								}) => (
+									<form onSubmit={handleSubmit}>
+										<Flex
+											alignContent={"center"}
+											alignItems={"center"}
+											justify={"space-between"}>
+											<Flex width={"100%"}>
+												<Select
+													onChange={(e) => {
+														setFieldValue("id", e.target.value);
+														setData(e.target.value);
+													}}
+													size="xs"
+													borderRadius={"10"}
+													name="greenhouse"
+													value={values.id}
+													placeholder="Pilih Greenhouse"
+													width={"100%"}
+													bg={"white"}
+													_active={{ bg: "white" }}
+													borderColor={"var(--color-border)"}
+													fontSize={"var(--header-5)"}
+													fontWeight={"normal"}
+													color={"var(--color-primer)"}
+													_hover={{ borderColor: "var(--color-border)" }}
+													_focusWithin={{ borderColor: "var(--color-border)" }}>
+													{dataApi.map((item, index) => {
+														return (
+															<option
+																color={"var(--color-border)"}
+																key={index}
+																value={item.id}>
+																{item.name}
+															</option>
+														);
+													})}
+												</Select>
+											</Flex>
+										</Flex>
+									</form>
+								)}
+							</Formik>
+						</Flex>
+						{data === "" ? (
+							<></>
+						) : (
+							<Link to={"/unit/controlling/add/" + data}>
+								<Button
+									data={{ name: data }}
+									type="submit"
+									bg={"var(--color-primer)"}>
+									Tambah
+								</Button>
+							</Link>
+						)}
+					</Flex>
+					{data === "" ? (
+						<></>
+					) : (
+						<TableControlling
+							data={{
+								id: data,
+							}}
+						/>
+					)}
+				</Flex>
+			)}
+		</>
 	);
 };
 export default Controlling;
