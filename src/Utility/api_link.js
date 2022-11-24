394fc69588608239e884d99b53df0c1c66d8bb55
Dhifaf Athiyah Zhabiyan <54610871+dhifafaz@users.noreply.github.com>
2022-11-24 14:28:34 +0700

    ganti link api

--- src/Utility/api_link.js
+++ src/Utility/api_link.js
@@ -1,51 +1,43 @@
-export const loginApi = "https://iterahero.herokuapp.com/api/v1/login";
-export const dashboardApi = "https://iterahero.herokuapp.com/api/v1/dashboard";
-export const greenhouseByUserId =
-	"https://iterahero.herokuapp.com/api/v1/greenhouse";
+export const loginApi = "https://iterahero.fly.dev/api/v1/login";
+export const dashboardApi = "https://iterahero.fly.dev/api/v1/dashboard";
+export const greenhouseByUserId = "https://iterahero.fly.dev/api/v1/greenhouse";
 export const controllingApi =
-	"https://iterahero.herokuapp.com/api/v1/actuator?by_greenhouse_id=";
+	"https://iterahero.fly.dev/api/v1/actuator?by_greenhouse_id=";
 export const monitoringApi =
-	"https://iterahero.herokuapp.com/api/v1/sensor?by_id_greenhouse=";
+	"https://iterahero.fly.dev/api/v1/sensor?by_id_greenhouse=";
 export const listGreenhouse =
-	"https://iterahero.herokuapp.com/api/v1/greenhouse?size=100";
-export const addGreenhouse =
-	"https://iterahero.herokuapp.com/api/v1/greenhouse";
-export const deleteGreenhouse =
-	"https://iterahero.herokuapp.com/api/v1/greenhouse/";
-export const updateGreenhouse =
-	"https://iterahero.herokuapp.com/api/v1/greenhouse/";
-export const getApiGreenhouse =
-	"https://iterahero.herokuapp.com/api/v1/greenhouse/";
-export const categoryApi =
-	"https://iterahero.herokuapp.com/api/v1/category/sensor";
-export const addSensorApi = "https://iterahero.herokuapp.com/api/v1/sensor";
-export const addActuatorApi = "https://iterahero.herokuapp.com/api/v1/actuator";
-export const deleteAktuatorApi =
-	"https://iterahero.herokuapp.com/api/v1/actuator/";
-export const deleteSensorApi = "https://iterahero.herokuapp.com/api/v1/sensor/";
+	"https://iterahero.fly.dev/api/v1/greenhouse?size=100";
+export const addGreenhouse = "https://iterahero.fly.dev/api/v1/greenhouse";
+export const deleteGreenhouse = "https://iterahero.fly.dev/api/v1/greenhouse/";
+export const updateGreenhouse = "https://iterahero.fly.dev/api/v1/greenhouse/";
+export const getApiGreenhouse = "https://iterahero.fly.dev/api/v1/greenhouse/";
+export const categoryApi = "https://iterahero.fly.dev/api/v1/category/sensor";
+export const addSensorApi = "https://iterahero.fly.dev/api/v1/sensor";
+export const addActuatorApi = "https://iterahero.fly.dev/api/v1/actuator";
+export const deleteAktuatorApi = "https://iterahero.fly.dev/api/v1/actuator/";
+export const deleteSensorApi = "https://iterahero.fly.dev/api/v1/sensor/";
 export const paginationMonitoring =
-	"https://iterahero.herokuapp.com/api/v1/sensor?by_id_greenhouse=";
+	"https://iterahero.fly.dev/api/v1/sensor?by_id_greenhouse=";
 export const paginationAktuator =
-	"https://iterahero.herokuapp.com/api/v1/actuator?by_greenhouse_id=";
+	"https://iterahero.fly.dev/api/v1/actuator?by_greenhouse_id=";
 export const getNotificationByUserId =
-	"https://iterahero.herokuapp.com/api/v1/notification?by_user_id=1";
-export const brokerSensor = 'https://iterahero.herokuapp.com/api/v1/sensor_broker?id_sensor='
-export const idSensor =  'https://iterahero.herokuapp.com/api/v1/sensor/'
+	"https://iterahero.fly.dev/api/v1/notification?by_user_id=1";
+export const brokerSensor =
+	"https://iterahero.fly.dev/api/v1/sensor_broker?id_sensor=";
+export const idSensor = "https://iterahero.fly.dev/api/v1/sensor/";
 
-export const getGrafikSensor = 'https://iterahero.herokuapp.com/api/v1/grafik/'
+export const getGrafikSensor = "https://iterahero.fly.dev/api/v1/grafik/";
 export const deleteNotification =
-	"https://iterahero.herokuapp.com/api/v1/notification/";
+	"https://iterahero.fly.dev/api/v1/notification/";
 
 export const updateActuatorDetail =
-	"https://iterahero.herokuapp.com/api/v1/actuator/";
+	"https://iterahero.fly.dev/api/v1/actuator/";
 
+export const updateSensorDetail = "https://iterahero.fly.dev/api/v1/sensor/";
 
-
-export const updateSensorDetail =
-	"https://iterahero.herokuapp.com/api/v1/sensor/";
-
-export const getLogAktuator = "https://iterahero.herokuapp.com/api/v1/actuator-log?by_actuator_id="
-export const postLogAktuator = 'https://iterahero.herokuapp.com/api/v1/actuator-log'
-export const icons = 'https://iterahero.herokuapp.com/api/v1/icon'
-export const Status = 'https://iterahero.herokuapp.com/api/v1/actuator-broker?id_actuator='
-
+export const getLogAktuator =
+	"https://iterahero.fly.dev/api/v1/actuator-log?by_actuator_id=";
+export const postLogAktuator = "https://iterahero.fly.dev/api/v1/actuator-log";
+export const icons = "https://iterahero.fly.dev/api/v1/icon";
+export const Status =
+	"https://iterahero.fly.dev/api/v1/actuator-broker?id_actuator=";
